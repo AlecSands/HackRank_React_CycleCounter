e9657eb1ade0d743314cbeb64721073a21495bdb
Alec Sands <sandsalecv@gmail.com>
2020-07-23 13:44:15 -0500

    solution 1

--- src/components/CycleCounter.js
+++ src/components/CycleCounter.js
@@ -1,13 +1,36 @@
 import React from 'react';
 
 class CycleCounter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cycleVal : this.props.cycle,
+      currentVal : 0
+    };
+    this.updateVal = this.updateVal.bind(this);
+  }
+
+  updateVal(event) {
+    // console.log(this.state.cycleVal);
+    let currentVal = this.state.currentVal;
+    const cycleVal = this.state.cycleVal;
+    currentVal++;
+    if (currentVal >= cycleVal) {
+      this.setState({currentVal: 0});
+    } else {
+      this.setState({currentVal: currentVal})
+    }
+  }
 
   render() {
+    const currentVal = this.state.currentVal;
+
     return (
       <button
         data-testid="cycle-counter"
         style={{ fontSize: '1rem', width: 120, height: 30, }}
-      >0</button>
+        onClick={this.updateVal}
+      >{currentVal}</button>
     );
   }
 }
